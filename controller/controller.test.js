83bd9b957475631edaaee882e035f24e7493dc2e
Mohamed Amine GUERRAS <fm_guerras@esi.dz>
2021-12-02 17:43:39 +0100

    test: update test

--- controller/controller.test.js
+++ controller/controller.test.js
@@ -7,7 +7,6 @@ test('uploading xml file to redis', async () => {
 
     redisClient.mSetAsync = jest.fn().mockReturnValue(true);
 
-    redisClient.quit = jest.fn().mockReturnValue(true);
     let res = await uplaodToRedis("config.xml");
     expect(res).toEqual(true);
 });
