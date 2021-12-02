67cdae2510b6bf2bc64ca4e46629d389176ddf45
Mohamed Amine GUERRAS <fm_guerras@esi.dz>
2021-12-02 17:38:44 +0100

    feat: add command line args

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const uplaodToRedis = require('./controller/controller.js');
 
-console.log(process.argv);
+if(process.argv[2] && process.argv[2] === '-v' && process.argv[3]){
+  uplaodToRedis(process.argv[3], true);
+}else{
+  if(process.argv[3]){
+    uplaodToRedis(process.argv[3], false);
+  }else{
+    console.log("Please provide the xml file path.");
+    process.exit();
+  }
+}
 
-uplaodToRedis('./config.xml', true);
